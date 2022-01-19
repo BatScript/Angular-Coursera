72f1caa217859a508732c93213cda18f70285c26
unknown <mohit80940@gmail.com>
2022-01-19 12:26:59 +0530

    directives

---
+++ 10 - Directives/app.js
@@ -0,0 +1,67 @@
+(function () {
+  "use strict";
+  angular
+    .module("my-app", [])
+    .controller("conOne", conOne)
+    .controller("conTwo", conTwo)
+    .service("iceAndFire", iceAndFire)
+    .directive("listItems", ListItems)
+    .directive("wholeList", WholeList)
+    .directive("conTwoList", ConTwoList);
+
+  function WholeList() {
+    var ddo = {
+      templateUrl: "characterList.html",
+    };
+    return ddo;
+  }
+  function ListItems() {
+    var ddo = {
+      restrict: "AE",
+      template: "<b>{{items.name}}</b> : {{items.occupation}} ",
+    };
+    return ddo;
+  }
+
+  function ConTwoList() {
+    var ddo = {
+      templateUrl: "newList.html",
+    };
+    return ddo;
+  }
+
+  function conTwo() {
+    var c2 = this;
+    c2.list = ["Mohit", "Potato", "Egypt", "Whale", "Aquaman"];
+    c2.delete = function (id) {
+      c2.list.splice(id, 1);
+    };
+  }
+
+  conOne.$inject = ["$q", "iceAndFire"];
+  function conOne($q, iceAndFire) {
+    var con = this;
+    con.apiData = "";
+    var deferred = $q.defer();
+    var promise = iceAndFire.fetchApi();
+    promise.then(function (response) {
+      con.apiData = response.data;
+    });
+    con.deadPeople = function (id) {
+      document.getElementById(id).style.textDecoration = "line-through";
+    };
+  }
+
+  iceAndFire.$inject = ["$http"];
+  function iceAndFire($http) {
+    var GOT = this;
+    GOT.fetchApi = function () {
+      var response = $http({
+        method: "GET",
+        url: "https://breakingbadapi.com/api/characters",
+      });
+
+      return response;
+    };
+  }
+})();
--- 9 - $http stuff/app.js
+++ 9 - $http stuff/app.js
@@ -9,12 +9,22 @@
   firstController.$inject = ["movieService"];
   function firstController(movieService) {
     var fc = this;
+
     var p = movieService.getFilms();
     p.then(function (response) {
       fc.value = response.data;
     }).catch(function (error) {
       fc.value = error;
     });
+
+    fc.aboutFilm = function (id) {
+      var p2 = movieService.getOneFilm(id);
+      p2.then(function (response) {
+        console.log(response);
+      }).catch(function (error) {
+        console.log(error);
+      });
+    };
   }
 
   movieService.$inject = ["$http"];
@@ -24,6 +34,13 @@
       var response = $http({
         method: "GET",
         url: "https://ghibliapi.herokuapp.com/films/",
+      });
+      return response;
+    };
+    service.getOneFilm = function (id) {
+      var response = $http({
+        method: "GET",
+        url: "https://ghibliapi.herokuapp.com/films/" + id,
       });
       return response;
     };
